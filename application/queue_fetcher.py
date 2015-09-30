288258479f6ab871b76455efd9f2cbab9e7a8fe4
mitchfriedman <mitchfriedman5@gmail.com>
2015-09-29 22:55:05 -0400

    Rename email_queue to queue_fetcher

--- application/queue_fetcher.py
+++ application/queue_fetcher.py
@@ -1,4 +1,14 @@
-class QueueFetcher(object):
-    def __init__(self):
-        pass
+from siphon import Client
+from siphon.queue import Queue
+
+
+class EmailQueue(object):
+    def __init__(self, siphon_client, queue_name):
+        self.client = siphon_client
+        self.queue_name = queue_name
+        self.emails = Queue(self.client.Queues, self.queue_name)
+    
+    def fetch(self):
+        return self.emails.dequeue()
+    
 
